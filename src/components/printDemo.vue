fb54da94bd57fb703100053801807aae8b552d3d
H5-ymm <1594363841@qq.com>
2020-03-08 16:45:14 +0800

    change

--- src/components/printDemo.vue
+++ src/components/printDemo.vue
@@ -10,22 +10,22 @@
       class="print-table"
       max-height="2000px"
     >
-      <el-table-column label="工程名称" prop="name" width="55px" align="center"></el-table-column>
-      <el-table-column label="工程地点" align="center" width="80px">
+      <el-table-column label="工程名称" prop="name" width="55" align="center"></el-table-column>
+      <el-table-column label="工程地点" align="center" width="80">
         <template slot-scope="scope">
           <span>{{scope.row.province}}{{scope.row.city}}{{scope.row.area}}{{scope.row.address}}</span>
         </template>
       </el-table-column>
-      <el-table-column label="项目类别" prop="type" width="55px" align="center"></el-table-column>
-      <el-table-column label="建筑规模" width="55px" prop="scale" align="center"></el-table-column>
-      <el-table-column label="工程造价" width="60px" prop="engineering_cost" align="center"></el-table-column>
-      <el-table-column label="施工许可证号" width="60px" align="center">
+      <el-table-column label="项目类别" prop="type" width="55" align="center"></el-table-column>
+      <el-table-column label="建筑规模" width="55" prop="scale" align="center"></el-table-column>
+      <el-table-column label="工程造价" width="60" prop="engineering_cost" align="center"></el-table-column>
+      <el-table-column label="施工许可证号" width="60" align="center">
         <template slot-scope="scope">
           <span>{{scope.row.make_license + ''}}</span>
         </template>
       </el-table-column>
-      <el-table-column label="结构类型" prop="structure_type" align="center" width="55px"></el-table-column>
-      <el-table-column label="建设单位" width="60px" align="center">
+      <el-table-column label="结构类型" prop="structure_type" align="center" width="55"></el-table-column>
+      <el-table-column label="建设单位" width="60" align="center">
         <template slot-scope="scope">
           <div class="cell-item">
             <p>{{scope.row.construction_unit}}</p>
@@ -33,7 +33,7 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column label="勘查单位" width="60px" align="center">
+      <el-table-column label="勘查单位" width="60" align="center">
         <template slot-scope="scope">
           <div class="cell-item">
             <p>{{scope.row.survey_unit}}</p>
@@ -41,7 +41,7 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column label="设计单位" width="60px" align="center">
+      <el-table-column label="设计单位" width="60" align="center">
         <template slot-scope="scope">
           <div class="cell-item">
             <p>{{scope.row.design_unit}}</p>
@@ -49,7 +49,7 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column label="施工单位" width="60px" align="center">
+      <el-table-column label="施工单位" width="60" align="center">
         <template slot-scope="scope">
           <div class="cell-item">
             <p>{{scope.row.shigong_unit}}</p>
@@ -57,7 +57,7 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column label="监理单位" width="60px" align="center">
+      <el-table-column label="监理单位" width="60" align="center">
         <template slot-scope="scope">
           <div class="cell-item">
             <p>{{scope.row.supervision_unit}}</p>
@@ -65,7 +65,7 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column label="检测单位" width="60px" align="center">
+      <el-table-column label="检测单位" width="60" align="center">
         <template slot-scope="scope">
           <div class="cell-item">
             <p>{{scope.row.detection_unit}}</p>
@@ -73,7 +73,7 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column label="商砼单位" width="60px" align="center">
+      <el-table-column label="商砼单位" width="60" align="center">
         <template slot-scope="scope">
           <div class="cell-item">
             <p>{{scope.row.commercialconcrete_unit}}</p>
@@ -81,12 +81,12 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column label="监督组" width="55px" align="center">
+      <el-table-column label="监督组" width="55" align="center">
         <template slot-scope="scope">
           <span>{{scope.row.monitoring_group + ''}}</span>
         </template>
       </el-table-column>
-      <!-- <el-table-column label="计划竣工日期" width="55px" align="center">
+      <!-- <el-table-column label="计划竣工日期" width="55" align="center">
       <template slot-scope="scope">
         <span>
           {{scope.row.endtime?
@@ -94,7 +94,7 @@
         </span>
       </template>
       </el-table-column>-->
-      <!-- <el-table-column label="层数" align="center" width="55px">
+      <!-- <el-table-column label="层数" align="center" width="55">
       <template slot-scope="scope">
         <span>{{scope.row.layers}}层</span>
       </template>
@@ -124,9 +124,8 @@ export default {
 .print-table.el-table td {
   font-size: 10px !important;
   padding: 0 !important;
-  width: 55px !important;
+  /* width: 55px !important; */
   color: #333 !important;
-  margin: 0 !important;
 }
 .print-table.el-table .cell-item {
   text-align: center;
@@ -134,16 +133,18 @@ export default {
 .print-table.el-table th {
   font-size: 10px !important;
   padding: 0 !important;
-  width: 55px !important;
-  margin: 0 !important;
+  /* width: 55px !important; */
 }
 .print-table.el-table th > .cell {
   padding-left: 0 !important;
   padding-right: 0 !important;
-  width: 55px !important;
+  width: 50px !important;
   line-height: 12px !important;
   font-size: 10px !important;
-  transform: scale(0.8) !important;
+  /* transform: scale(0.8) !important; */
+}
+.print-table.el-table .cell span {
+  font-size: 10px !important;
 }
 .print-table.el-table tbody td {
   padding: 6px 0 !important;
@@ -152,10 +153,10 @@ export default {
 .print-table.el-table td > .cell {
   padding-left: 0 !important;
   padding-right: 0 !important;
-  width: 55px !important;
+  width: 50px !important;
   line-height: 12px !important;
   font-size: 10px !important;
-  transform: scale(0.8) !important;
+  /* transform: scale(0.8) !important; */
 }
 .print-table.el-table td:nth-child(2) > .cell {
   padding-left: 0 !important;
